9113bf60b30327c8b55d7220433c0b57c16cc17e
Mark Nguyen <nguyenmp605@gmail.com>
2012-11-20 19:27:53 -0800

    Uses fragment_list.xml instead of the default list fragment Shows progress bar for "My Courses" Added refresh

--- src/com/nguyenmp/gauchodroid/courses/MyCoursesFragment.java
+++ src/com/nguyenmp/gauchodroid/courses/MyCoursesFragment.java
@@ -20,23 +20,26 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.Vibrator;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockListFragment;
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.nguyenmp.gauchodroid.R;
 import com.nguyenmp.gauchodroid.browser.BrowserActivity;
 import com.nguyenmp.gauchodroid.common.HandledThread;
+import com.nguyenmp.gauchodroid.common.MenuUtils;
 import com.nguyenmp.gauchodroid.course.CourseActivity;
 import com.nguyenmp.gauchodroid.login.LoginActivity;
 import com.nguyenmp.gauchodroid.login.LoginManager;
@@ -44,11 +47,16 @@ import com.nguyenmp.gauchospace.GauchoSpaceClient;
 import com.nguyenmp.gauchospace.thing.Course;
 import com.nguyenmp.gauchospace.thing.Instructor;
 
-public class MyCoursesFragment extends SherlockListFragment {
+public class MyCoursesFragment extends SherlockFragment implements MyCoursesDownloadListener, OnItemClickListener, OnItemLongClickListener {
 	private BaseAdapter mListAdapter;
 	private List<Course> mCourseList;
+	private String mText;
 	private boolean mLoaded = false;
 	private static final String COURSE_LIST_KEY = "KOURSE LCISKD KEY";
+	private Context mContext;
+	private ListView mListView;
+	private ProgressBar mProgressBar;
+	private TextView mTextView;
 	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
@@ -56,57 +64,64 @@ public class MyCoursesFragment extends SherlockListFragment {
 	}
 	
 	@Override
-	public void onActivityCreated(Bundle inState) {
-		super.onActivityCreated(inState);
-		super.registerForContextMenu(getListView());
-	}
-	
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle inState) {
+		//Inflate our custom list view layout
+		View inflatedView = inflater.inflate(R.layout.fragment_list, container, false);
+		
+		//Must be called in order to show our options menu in the action bar
+		super.setHasOptionsMenu(true);
+		
+		//Get member views
+		mListView = (ListView) inflatedView.findViewById(R.id.fragment_list_list_view);
+		mProgressBar = (ProgressBar) inflatedView.findViewById(R.id.fragment_list_progress_bar);
+		mTextView = (TextView) inflatedView.findViewById(R.id.fragment_list_text_view);
+		
+		mContext = getActivity();
+		mCourseList = new ArrayList<Course>();
+		mListAdapter = new CourseListAdapter(mCourseList, mContext);
+		mListView.setAdapter(mListAdapter);
+		mListView.setOnItemClickListener(this);
+		mListView.setOnItemLongClickListener(this);
+		super.registerForContextMenu(mListView);
+		
 		if (inState != null && inState.containsKey(COURSE_LIST_KEY)) {
-			mCourseList = (List<Course>) inState.getSerializable(COURSE_LIST_KEY);
-			mListAdapter = new CourseListAdapter(mCourseList, getActivity());
-			
-			setListAdapter(mListAdapter);
-			mLoaded = true;
+			List<Course> courses = (List<Course>) inState.getSerializable(COURSE_LIST_KEY);
+			onDownloaded(courses, "Error");  //If we somehow stored a null list, then show error
 		} else {
-			mCourseList = new ArrayList<Course>();
-			mListAdapter = new CourseListAdapter(mCourseList, getActivity());
-			
-			setListAdapter(mListAdapter);
-			CookieStore cookies = LoginManager.getCookies(getSherlockActivity());
-	
-			Handler coursesDownloadHandler = new CoursesDownloadHandler(this, mCourseList, mListAdapter);
-			Thread coursesDownloadThread = new CoursesDownloadThread(coursesDownloadHandler, cookies);
-			coursesDownloadThread.start();
+			refresh();
 		}
 		
-		return super.onCreateView(inflater, container, inState);
+		return inflatedView;
+	}
+	
+	private void refresh() {
+		onDownloaded(null, null); //Show progress
+		CookieStore cookies = LoginManager.getCookies(mContext);
+		
+		Handler coursesDownloadHandler = new CoursesDownloadHandler(this, mContext);
+		Thread coursesDownloadThread = new CoursesDownloadThread(coursesDownloadHandler, cookies);
+		coursesDownloadThread.start();
 	}
 	
 	@Override
-	public void onListItemClick(ListView listView, View view, int position, long id) {
+	public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
 		Course course = mCourseList.get(position);
 		Uri uri = Uri.parse(course.getUrl());
-		Intent intent = new Intent(getActivity(), CourseActivity.class);
+		Intent intent = new Intent(mContext, CourseActivity.class);
 		intent.putExtra(CourseActivity.EXTRA_COURSE_ID, Integer.parseInt(uri.getQueryParameter("id")));
 		intent.putExtra(CourseActivity.EXTRA_TITLE, course.getName() + ": " + course.getTitle());
-		getActivity().startActivity(intent);
+		mContext.startActivity(intent);
 	}
 	
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo info) {
-		super.onCreateContextMenu(menu, view, info);
-		
-		final int position = ((AdapterContextMenuInfo) info).position;
-		
+	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 		final Course course = mCourseList.get(position);
 		final String name = course.getName();
 		final String title = course.getTitle();
 		final String summary = course.getSummary();
 		final Instructor[] instructors = course.getInstructors().toArray(new Instructor[] {});
 		
-		Dialog dialog = new Dialog(getActivity());
+		Dialog dialog = new Dialog(mContext);
 		dialog.setTitle(name + ": " + title);
 		dialog.setContentView(R.layout.dialog_course_description);
 		
@@ -115,7 +130,7 @@ public class MyCoursesFragment extends SherlockListFragment {
 		
 		LinearLayout instructorGroup = (LinearLayout) dialog.findViewById(R.id.dialog_course_description_instructors);
 		for (final Instructor instructor : instructors) {
-			final Button button = new Button(getActivity());
+			final Button button = new Button(mContext);
 			button.setText(instructor.getName());
 			button.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
@@ -129,10 +144,64 @@ public class MyCoursesFragment extends SherlockListFragment {
 		
 		dialog.show();
 		
-		Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-		vibrator.vibrate(50);
+		
+		
+		return true;
 	}
 
+	@Override
+	public void onDownloaded(List<Course> courses, String message) {
+		mLoaded = (courses != null);
+		mCourseList.clear();
+		
+		if (courses == null) {
+			if (message == null) {
+				mText = null;
+				mProgressBar.setVisibility(View.VISIBLE);
+				mTextView.setVisibility(View.GONE);
+				mListView.setVisibility(View.GONE);
+			} else {
+				mText = message;
+				mTextView.setText(mText);
+				mTextView.setVisibility(View.VISIBLE);
+				mProgressBar.setVisibility(View.GONE);
+				mListView.setVisibility(View.GONE);
+			}
+		} else {
+			if (courses.isEmpty()) {
+				mText = "You Are Not Enrolled In Any Courses";
+				mTextView.setText(mText);
+				mTextView.setVisibility(View.VISIBLE);
+				mProgressBar.setVisibility(View.GONE);
+				mListView.setVisibility(View.GONE);
+			} else {
+				mText = null;
+				mTextView.setVisibility(View.GONE);
+				mProgressBar.setVisibility(View.GONE);
+				mCourseList.addAll(courses);
+				mListView.setVisibility(View.VISIBLE);
+			}
+		}
+		mListAdapter.notifyDataSetChanged();
+	}
+	
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		
+		MenuUtils.addMenuItem(menu, "Refresh").setIcon(R.drawable.ic_menu_refresh).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getTitle().equals("Refresh")) {
+			refresh();
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
 	private static class CoursesDownloadThread extends HandledThread {
 		private CookieStore mCookies;
 		
@@ -172,31 +241,27 @@ public class MyCoursesFragment extends SherlockListFragment {
 	}
 	
 	private static class CoursesDownloadHandler extends Handler {
+		private final MyCoursesDownloadListener mListener;
 		private final Context mContext;
-		private final List<Course> mCourseList;
-		private final BaseAdapter mListAdapter;
-		private final MyCoursesFragment mFragment;
 		
-		CoursesDownloadHandler(MyCoursesFragment fragment, List<Course> courseList, BaseAdapter listAdapter) {
-			mContext = fragment.getActivity();
-			mCourseList = courseList;
-			mListAdapter = listAdapter;
-			mFragment = fragment;
+		CoursesDownloadHandler(MyCoursesDownloadListener listener, Context context) {
+			mListener = listener;
+			mContext = context;
 		}
 		
 		@Override
 		public void handleMessage(Message message) {
-			mFragment.mLoaded = true;
-			
 			if (message.obj instanceof CoursesPayload) {
 				List<Course> courses = ((CoursesPayload) message.obj).getPayload();
-				mCourseList.addAll(courses);
-				mListAdapter.notifyDataSetChanged();
+				mListener.onDownloaded(courses, null);
 			} else if (message.obj instanceof NullPointerException) {
 				LoginManager.setCookies(mContext, null);
 				Intent intent = new Intent(mContext, LoginActivity.class);
 				mContext.startActivity(intent);
-				//TODO:  Handle null pointers
+			} else if (message.obj instanceof Exception) {
+				mListener.onDownloaded(null, ((Exception) message.obj).toString());
+			} else {
+				mListener.onDownloaded(null, "Unknown error has occured.");
 			}
 		}
 	}
